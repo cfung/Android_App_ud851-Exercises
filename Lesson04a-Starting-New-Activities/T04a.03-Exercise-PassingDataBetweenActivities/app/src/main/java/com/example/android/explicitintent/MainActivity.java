6b8aa833f52a28838f85286cfbf19a6b14449e1a
cfung <chesterfung@gmail.com>
2017-07-20 23:23:17 -0700

    TODO 1

--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -53,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
              */
             @Override
             public void onClick(View v) {
-                // TODO (1) Retrieve the text from the EditText and store it in a variable
+                // compelted (1) Retrieve the text from the EditText and store it in a variable
+                String mText = mNameEntry.toString();
 
                 /*
                  * Storing the Context in a variable in this case is redundant since we could have
