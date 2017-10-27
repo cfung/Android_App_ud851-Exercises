f7e4d9aadd73d87ca858ca7699268a6fed7057d2
cfung <chesterfung@gmail.com>
2017-10-26 23:05:38 -0700

    complete TODO 4-6

--- Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -13,19 +13,19 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        // Load the preferences from an XML resource
-        setPreferencesFromResource(R.xml.pref_visualizer, rootKey);
+
+        // complleted (5) In SettingsFragment's onCreatePreferences method add the preference file using the
+        // addPreferencesFromResource method
+        addPreferencesFromResource(R.xml.pref_visualizer);
     }
 
 
 }
 // completed (3) In res->xml create a file called pref_visualizer
-// TODO (4) In pref_visualizer create a preference screen containing a single check box preference
+// completed (4) In pref_visualizer create a preference screen containing a single check box preference
 // This check box preference should have a default value of true, the key 'show_bass', a
 // summaryOff of Hidden, a summaryOn of Shown and a title of 'Show Bass'
 
-// TODO (5) In SettingsFragment's onCreatePreferences method add the preference file using the
-// addPreferencesFromResource method
 
 
 // TODO (7) Set the root layout of activity_settings to our newly created SettingsFragment
