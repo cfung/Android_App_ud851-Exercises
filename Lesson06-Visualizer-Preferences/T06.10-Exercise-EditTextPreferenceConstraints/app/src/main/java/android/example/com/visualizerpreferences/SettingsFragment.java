ccbade72b93b9e832dc71eef45b5e6b6a8eddf20
cfung <chesterfung@gmail.com>
2017-11-28 23:19:24 -0800

    complete TODO 1-6

--- Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ Lesson06-Visualizer-Preferences/T06.10-Exercise-EditTextPreferenceConstraints/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -27,9 +27,9 @@ import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
 import android.widget.Toast;
 
-// TODO (1) Implement OnPreferenceChangeListener
+// completed (1) Implement OnPreferenceChangeListener
 public class SettingsFragment extends PreferenceFragmentCompat implements
-        OnSharedPreferenceChangeListener {
+        OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
@@ -92,6 +92,28 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
     // to a float; if it cannot, show a helpful error message and return false. If it can be converted
     // to a float check that that float is between 0 (exclusive) and 3 (inclusive). If it isn't, show
     // an error message and return false. If it is a valid number, return true.
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+        Toast error = Toast.makeText(getContext(), "Please select a number between 0.1 and 3", Toast.LENGTH_SHORT);
+
+        String sizeKey = "size";
+        if (preference.getKey().equals(sizeKey)){
+            String stringSize = (String) newValue;
+            try{
+                float size = Float.parseFloat(stringSize);
+                if (size > 3 || size <=0){
+                    error.show();
+                    return false;
+                }
+            }catch (NumberFormatException nfe){
+                error.show();
+                return false;
+            }
+
+        }
+        return true;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
