ad5cb77715f434639ba697dc5da688a8633d7107
cfung <chesterfung@gmail.com>
2017-12-01 10:28:44 -0800

    complete TODO 1-5 and 8

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -1,22 +1,40 @@
 package com.example.android.waitlist.data;
 
-// TODO (1) extend the SQLiteOpenHelper class
-public class WaitlistDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-    // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
+// completed (1) extend the SQLiteOpenHelper class
+public class WaitlistDbHelper extends SQLiteOpenHelper{
 
-    // TODO (3) Create a static final int called DATABASE_VERSION and set it to 1
+    // completed (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
+    public static final String DATABASE_NAME = "waitlist.db";
 
-    // TODO (4) Create a Constructor that takes a context and calls the parent constructor
+    // completed (3) Create a static final int called DATABASE_VERSION and set it to 1
+    public static final int DATABASE_VERSION = 1;
 
-    // TODO (5) Override the onCreate method
+    // completed (4) Create a Constructor that takes a context and calls the parent constructor
+    public WaitlistDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    // completed (5) Override the onCreate method
+    @Override
+    public void onCreate(SQLiteDatabase db) {
 
         // TODO (6) Inside, create an String query called SQL_CREATE_WAITLIST_TABLE that will create the table
 
         // TODO (7) Execute the query by calling execSQL on sqLiteDatabase and pass the string query SQL_CREATE_WAITLIST_TABLE
 
-    // TODO (8) Override the onUpgrade method
+    }
+
+    // completed (8) Override the onUpgrade method
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
         // TODO (9) Inside, execute a drop table query, and then call onCreate to re-create it
+
+
+    }
 
 }
