44185790b91ca336052d2414ae5a33dcd2b3d03c
cfung <chesterfung@gmail.com>
2018-11-02 15:38:11 -0700

    complete tasks 1 to 10

--- Lesson09b-ToDo-List-AAC/T09b.02-Exercise-SaveTaskInDatabaseFromAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.02-Exercise-SaveTaskInDatabaseFromAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -16,6 +16,7 @@
 
 package com.example.android.todolist;
 
+import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -23,8 +24,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
+import com.example.android.todolist.database.AppDatabase;
 import com.example.android.todolist.database.TaskEntry;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class AddTaskActivity extends AppCompatActivity {
@@ -48,7 +55,8 @@ public class AddTaskActivity extends AppCompatActivity {
 
     private int mTaskId = DEFAULT_TASK_ID;
 
-    // TODO (3) Create AppDatabase member variable for the Database
+    // completed (3) Create AppDatabase member variable for the Database
+    AppDatabase appDatabase;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,7 +64,9 @@ public class AddTaskActivity extends AppCompatActivity {
 
         initViews();
 
-        // TODO (4) Initialize member variable for the data base
+        // completed (4) Initialize member variable for the data base
+        //appDatabase = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, ).build();
+        appDatabase = AppDatabase.getInstance(getApplicationContext());
 
         if (savedInstanceState != null && savedInstanceState.containsKey(INSTANCE_TASK_ID)) {
             mTaskId = savedInstanceState.getInt(INSTANCE_TASK_ID, DEFAULT_TASK_ID);
@@ -107,13 +117,30 @@ public class AddTaskActivity extends AppCompatActivity {
      * It retrieves user input and inserts that new task data into the underlying database.
      */
     public void onSaveButtonClicked() {
-        // TODO (5) Create a description variable and assign to it the value in the edit text
-        // TODO (6) Create a priority variable and assign the value returned by getPriorityFromViews()
-        // TODO (7) Create a date variable and assign to it the current Date
+        // completed (5) Create a description variable and assign to it the value in the edit text
+        String description = mEditText.getText().toString();
 
-        // TODO (8) Create taskEntry variable using the variables defined above
-        // TODO (9) Use the taskDao in the AppDatabase variable to insert the taskEntry
-        // TODO (10) call finish() to come back to MainActivity
+        // completed (6) Create a priority variable and assign the value returned by getPriorityFromViews()
+        int priority = getPriorityFromViews();
+
+        // compelted (7) Create a date variable and assign to it the current Date
+        Calendar calendar = Calendar.getInstance().getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = Calendar.getInstance().getTime();
+
+        // completed (8) Create taskEntry variable using the variables defined above
+        TaskEntry taskEntry = new TaskEntry(description, priority, date);
+
+        // completed (9) Use the taskDao in the AppDatabase variable to insert the taskEntry
+        appDatabase.taskDao().insertTask(taskEntry);
+
+        // completed (10) call finish() to come back to MainActivity
+        finish();
+
+
+
+        //public TaskEntry(String description, int priority, Date updatedAt)
+
     }
 
     /**
